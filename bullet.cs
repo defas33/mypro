a6e489f87097b94794f692c543afc4ebb7f15b36
defas33 <50552108+defas33@users.noreply.github.com>
2020-01-16 20:33:34 +0900

    hell

---
+++ bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public float distance;
+    public int damage;
+    public LayerMask checksolid;
+
+    public GameObject destroyEffect;
+
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+
+    void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.left, distance, checksolid);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("shot!");
+                hitInfo.collider.GetComponent<enemy>().TakeDamage(damage);
+            }
+            DestroyProjectile();
+
+        }
+
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+    }
+
+    void DestroyProjectile()
+    {
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
